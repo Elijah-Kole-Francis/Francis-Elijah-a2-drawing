89fefa98d61fc51eff2b6f54abea6160318c346d
elija <elija@LOUBITAN>
2024-10-02 21:07:05 -0400

    Put in a loop, put in another somewhere and try an array next

--- francis-elijah-a2-drawin/Game.cs
+++ francis-elijah-a2-drawin/Game.cs
@@ -12,14 +12,14 @@ namespace francis_elijah_a2_drawin
     public class Game
     {
         // Place your variables here:
+        Color colorRandom = Random.Color();
 
-        Color Brown = new Color(113, 56, 56);
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-          Window.SetTitle("Sword?.v2.1");
+          Window.SetTitle("Sword, new and improved");
           Window.SetSize(800, 600);
 
             
@@ -42,26 +42,32 @@ namespace francis_elijah_a2_drawin
           Draw.Quad(450,200,500,250,250,450,200,400);
           Draw.FillColor = Color.LightGray;
 
-
-            
             //Crossguard
             
           Draw.Quad(420,130,570,280,550,300,400,150);
           Draw.FillColor = new Color(113,56,56);
 
-            
 
             //Handle
+
             Draw.FillColor = new Color(113, 56, 56);
             Draw.Quad(580,100,610,130,510,220,480,190);
 
+            
+            Draw.LineSize = 5; 
+            Draw.LineColor = Color.Black;
+            Draw.Line(510,220,510,165);
+            Draw.Line(510, 165, 565, 165);
+            Draw.Line(565, 165, 565, 115);
+            
 
-
-            //Tip
+            //Tip and blood smear
+            Draw.LineSize = 1;
             Draw.FillColor = Color.Red;
             Draw.Triangle(200,400,250,450,170,470);
-            //Pommel and jewels
+            Draw.Triangle(230, 430, 300, 410, 250, 450);
 
+            //Pommel and jewels
             DrawJewel(410, 145);
             DrawJewel(600, 110);
             DrawJewel(570, 300);
@@ -90,12 +96,24 @@ namespace francis_elijah_a2_drawin
                 Draw.LineColor = Color.Black;
                 Draw.Circle(x, y, 20);
 
-                Draw.FillColor = Color.Green;
+                Draw.FillColor = colorRandom;
                 Draw.Circle(x, y, 10);
 
                 Draw.FillColor = Color.White;
                 Draw.Circle(x + 5, y - 3, 3);
 
+            }
+            
+            //Gonna see if I can draw a blood droplet in a certain area equal to the input number
+            //maybe I'll just do console interactivity
+            
+            Console.WriteLine("How many enemies have been felled by the sword?");
+            int enemyFelled = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= enemyFelled; i++)
+            {
+                int bloodiedBlade = enemyFelled * i + 100;
+                Console.WriteLine($"What a mighty warrior you are! You felled" + bloodiedBlade +" enemies!");
+                
             }
 
         }
